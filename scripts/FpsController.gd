c4dbc1986591428a724d91e91f41dba1449f0471
Sergey Kulikov <inffinity1@gmail.com>
2023-03-06 00:42:21 +0300

    buggy need some fix

--- scripts/FpsController.gd
+++ scripts/FpsController.gd
@@ -1,14 +1,24 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var cameraBasePoint := $CameraBasePoint
-@onready var camera := $CameraBasePoint/Camera3D
+@export var max_speed: float = 10
+@export var acceleration: float = 70
+@export var friction: float = 60
+@export var air_friction: float = 10
+@export var jump_impolse: float = 5
+@export var mouse_sensitivity: float = .01
+@export var controller_sensitivity: float = 3
+@export var camera_angle_bottom: float = -45
+@export var camera_angle_up: float = 45
 
+#nodes
+@onready var head := $Head
+@onready var camera := $Head/Camera3D
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -17,29 +27,62 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			cameraBasePoint.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+			head.rotate_y(-event.relative.x * mouse_sensitivity)
+			camera.rotate_x(-event.relative.y * mouse_sensitivity)
+			
+func apply_controller_rotation():
+	var axis_vector = Vector2.ZERO
+	axis_vector.x = Input.get_action_strength('look_right') - Input.get_action_strength('look_left')
+	axis_vector.y = Input.get_action_strength('look_down') - Input.get_action_strength('look_up')
+	
+	if InputEventJoypadMotion:
+		rotate_y(deg_to_rad(-axis_vector.x) * controller_sensitivity)
+		head.rotate_x(deg_to_rad(-axis_vector.y) * controller_sensitivity)
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed('Jump') and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	var input_dir = Input.get_vector('Left', 'Right', 'Forward', 'Back')
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	var input_vector = get_input_vector()
+	var direction = get_direction(input_vector)
+	apply_movement(direction, delta)
+	apply_friction(direction, delta)
+	apply_gravity(delta)
+	jump()
+	apply_controller_rotation()
+	head.rotation.x = clamp(head.rotation.x, deg_to_rad(camera_angle_bottom), deg_to_rad(camera_angle_up))
 	move_and_slide()
+	
+	
+func get_input_vector():
+	var input_vector = Input.get_vector('move_left', 'move_right', 'move_forward', 'move_back')
+	return input_vector.normalized() if input_vector.length() > 1 else input_vector
+	
+func get_direction(input_vector: Vector3):
+	var direction = Vector3.ZERO
+	direction = (input_vector.x * transform.basis.x) + (input_vector.z * transform.basis.z)
+	return direction.normalized()
+	
+func apply_movement(direction: Vector3, delta: float):
+	if direction != Vector3.ZERO:
+		velocity.x = velocity.move_toward(direction * max_speed, acceleration * delta).x
+		velocity.z = velocity.move_toward(direction * max_speed, acceleration * delta).z
+		
+func apply_friction(direction: Vector3, delta: float):
+	if direction == Vector3.ZERO:
+		if is_on_floor():
+			velocity = velocity.move_toward(Vector3.ZERO, friction * delta)
+		else:
+			velocity.x = velocity.move_toward(Vector3.ZERO, air_friction * delta).x
+			velocity.z = velocity.move_toward(Vector3.ZERO, air_friction * delta).z
+			
+func apply_gravity(delta: float):
+	velocity.y += gravity * delta
+	velocity.y = clamp(velocity.y, gravity, jump_impolse)
+	
+	
+func jump():
+	if Input.is_action_just_pressed('jump') and is_on_floor():
+		velocity.y = jump_impolse
+	if Input.is_action_just_released('jump') and velocity.y > jump_impolse / 2:
+		velocity.y = jump_impolse / 2
+	
+
+	
