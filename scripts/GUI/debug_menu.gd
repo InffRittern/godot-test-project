c686a8ba021e00950fca826454a505d3d5d6773b
Sergey Kulikov <inffinity1@gmail.com>
2023-03-09 21:31:10 +0300

    update debug menu script

--- scripts/GUI/debug_menu.gd
+++ scripts/GUI/debug_menu.gd
@@ -2,6 +2,7 @@ extends ColorRect
 
 var max_fps: int = 0
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
@@ -17,14 +18,12 @@ func _unhandled_input(event):
 
 func _on_spin_box_value_changed(value):
 	max_fps = value
-	pass # Replace with function body.
 
-
+# set engine max fps value
 func _on_check_button_toggled(button_pressed):
 	Engine.set_max_fps(max_fps if button_pressed else 0)
-	pass # Replace with function body.
 
-
+# handling quit event
 func _on_quit_pressed():
 	get_tree().quit()
-	pass # Replace with function body.
+
