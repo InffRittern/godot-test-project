111fff2bcb35d9c3b205418c75c6c0d8cb36b314
Sergey Kulikov <ksa@raa-st.com>
2023-03-09 14:33:32 +0300

    update debug menu

--- scripts/GUI/debug_menu.gd
+++ scripts/GUI/debug_menu.gd
@@ -1,12 +1,10 @@
 extends ColorRect
 
-@onready var curent_fps := $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Current_fps
-@onready var fps_slider := $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/HSlider
+var max_fps: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
-	curent_fps.text = 'FPS: ' + str(fps_slider.value)
 	pass # Replace with function body.
 
 
@@ -16,6 +14,12 @@ func _unhandled_input(event):
 		Engine.set_time_scale(0.0 if visible else 1.0)
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if visible else Input.MOUSE_MODE_CAPTURED)
 
-func _on_h_slider_value_changed(value):
-	Engine.set_max_fps(value)
-	curent_fps.text = 'FPS: ' + str(value)
+
+func _on_spin_box_value_changed(value):
+	max_fps = value
+	pass # Replace with function body.
+
+
+func _on_check_button_toggled(button_pressed):
+	Engine.set_max_fps(max_fps if button_pressed else 0)
+	pass # Replace with function body.
